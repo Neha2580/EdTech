a5d9bc46b8b69118eb27f5ef3c68e2a52158473c
Neha Sahu <nehasahu160802@gmail.com>
2025-03-06 22:02:08 +0530

    Added events application

---
+++ hospital_project/events/views.py
@@ -0,0 +1,9 @@
+# from django.shortcuts import render
+from django.template import loader 
+from django.http import HttpResponse
+
+def event1(request):
+    template = loader.get_template('home_page.html')
+    return HttpResponse(template.render())
+
+# Create your views here.
